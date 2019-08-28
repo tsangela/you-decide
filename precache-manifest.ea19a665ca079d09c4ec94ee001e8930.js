8a5596ecd374acaca695143c5ceb9ce4788f6190
Angela Tsang <tsangangela_@hotmail.com>
2019-08-28 00:35:31 -0700

    Updates

---
+++ precache-manifest.ea19a665ca079d09c4ec94ee001e8930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f17580ecb5cf7715edf925544ef339",
+    "url": "/you-decide/index.html"
+  },
+  {
+    "revision": "c4fd560cb2b4fe19a6c0",
+    "url": "/you-decide/static/css/main.40037d52.chunk.css"
+  },
+  {
+    "revision": "a9cc78a78ff5fe11d1ce",
+    "url": "/you-decide/static/js/2.52d29b59.chunk.js"
+  },
+  {
+    "revision": "c4fd560cb2b4fe19a6c0",
+    "url": "/you-decide/static/js/main.574f53b6.chunk.js"
+  },
+  {
+    "revision": "386f27ee55801e339bae",
+    "url": "/you-decide/static/js/runtime~main.947e831f.js"
+  }
+]);
